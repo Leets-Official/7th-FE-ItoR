3bc38cd263044a6807a98a362006caa1720768af
Tnalxmsk <alstnwkd990@naver.com>
2026-03-20 18:41:29 +0900

    chore: tailwind, 스타일 설정 및 cn 유틸함수 구현

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,5 +1,14 @@
+import '@apps/styles/styles.css'
+import { cn } from '@shared/utils'
+
 function App() {
-  return <div>initial</div>
+  const exampleClass = 'bg-red-500'
+  return (
+    <div className={cn('flex flex-col', exampleClass)}>
+      <h2>initial</h2>
+      <p>Example of using the cn utility function to merge class names.</p>
+    </div>
+  )
 }
 
 export default App
