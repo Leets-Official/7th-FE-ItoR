88631a3c575ee5f240c8d968d3796628653be078
251207 <ethan.jang@obigo.com>
2026-03-20 17:28:49 +0900

    chore: 프로젝트 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@apps': path.resolve(__dirname, './src/apps'),
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 })
