d8040673d100b0cd6651ea5afc9fd49b79019059
Tnalxmsk <alstnwkd990@naver.com>
2026-03-22 19:25:17 +0900

    feat: 게시글 목록 조회 기능 구현

--- src/pages/post/post-list/PostListPage.tsx
+++ src/pages/post/post-list/PostListPage.tsx
@@ -1,5 +1,14 @@
+import { FetchBoundary } from '@shared/ui';
+import PostList from '@pages/post/post-list/components/post-list/PostList.tsx';
+
 function PostListPage() {
-  return <div>PostListPage</div>;
+  return (
+    <div>
+      <FetchBoundary loadingFallback={<div>게시글 목록 가져오는중</div>}>
+        <PostList />
+      </FetchBoundary>
+    </div>
+  );
 }
 
 export default PostListPage;
