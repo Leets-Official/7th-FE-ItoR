bd530ad5d323117656d5c860750222d86ad1a9cd
YeBeenChoi <yebin607@naver.com>
2026-03-31 22:45:45 +0900

    feat: common icons svg 파일  추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,1 +1,14 @@
-export { DoneIcon, ErrorIcon } from './common';
+export {
+  AddPhotoAlternateIcon,
+  ChatIcon,
+  ClearIcon,
+  CreateIcon,
+  DeleteForeverIcon,
+  DoneIcon,
+  ErrorOutlineIcon,
+  FolderOpenIcon,
+  MoreVertIcon,
+  NavigateBeforeIcon,
+  ReorderIcon,
+  SettingsIcon,
+} from './common';
