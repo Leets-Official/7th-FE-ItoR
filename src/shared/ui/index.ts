0896fb228566239aacca4b0ce591f277d20e5eb2
Tnalxmsk <alstnwkd990@naver.com>
2026-04-06 17:07:16 +0900

    feat: export 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,1 +1,6 @@
 export { FetchErrorFallback, FetchBoundary } from './boundary';
+export { Button, IconButton } from './button';
+export { Modal, ConfirmModal } from './modal';
+export type { ButtonProps } from './button';
+export type { IconButtonProps } from './button';
+export type { ConfirmModalProps } from './modal';
