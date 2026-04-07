5552386599166e95f24dae8f651401f665d17dbc
YeBeenChoi <yebin607@naver.com>
2026-04-07 23:34:59 +0900

    feat: Button 컴포넌트 추가

---
+++ src/components/common/Button/Button.types.ts
@@ -0,0 +1,13 @@
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
+
+import type { ButtonVariantProps } from './Button.variants';
+
+export interface ButtonProps
+  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'disabled'>,
+    Omit<ButtonVariantProps, 'state'> {
+  children?: ReactNode;
+  showIcon?: boolean;
+  icon?: ReactNode;
+  pressed?: boolean;
+  disabled?: boolean;
+}
