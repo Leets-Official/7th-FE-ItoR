46a85e3ea25534ad561f527a2e66fc465b32ac35
YeBeenChoi <yebin607@naver.com>
2026-04-28 23:54:21 +0900

    mod: Header 컴포넌트 상단 로고 변경

--- src/components/common/PageHeader/PageHeaderLeft.tsx
+++ src/components/common/PageHeader/PageHeaderLeft.tsx
@@ -1,4 +1,4 @@
-import { ReorderIcon } from '@/assets/icons/common';
+import { GitlogLogoIcon, ReorderIcon } from '@/assets/icons/common';
 import { FC } from 'react';
 
 const PageHeaderLeft: FC = () => {
@@ -6,12 +6,7 @@ const PageHeaderLeft: FC = () => {
     <div className="flex items-center justify-center gap-2">
       <div className="flex items-center gap-2.5">
         <ReorderIcon />
-        <div
-          className="flex items-center justify-center px-1.5 py-1.5 text-xl font-normal leading-[140%] text-black"
-          style={{ fontFamily: 'Smooch, cursive' }}
-        >
-          Gitlog
-        </div>
+        <GitlogLogoIcon className="h-[37px] w-[97px]" />
       </div>
     </div>
   );
