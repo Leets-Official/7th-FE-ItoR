66b3a232529f32bb2af49fbd0fa9a8aa23a51ef6
YeBeenChoi <yebin607@naver.com>
2026-04-28 23:53:56 +0900

    feat: 라우팅 설정 변경

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { BlogSearchPage } from '@/pages/BlogSearchPage';
 import { PlaygroundPage } from '@/pages/PlaygroundPage';
 
 export function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/playground" replace />} />
+      <Route path="/" element={<Navigate to="/blog-search" replace />} />
+      <Route path="/blog-search" element={<BlogSearchPage />} />
       <Route path="/playground" element={<PlaygroundPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
