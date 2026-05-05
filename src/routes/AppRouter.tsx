5a59ae5cea7d37db30f2e6d4cffb39ae5034a92b
YeBeenChoi <yebin607@naver.com>
2026-05-05 23:35:02 +0900

    feat: /mypage 라우트 추가

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { BlogSearchPage } from '@/pages/BlogSearchPage';
+import { MyPageJoinPage } from '@/pages/MyPageJoinPage';
 import { PlaygroundPage } from '@/pages/PlaygroundPage';
 
 export function AppRouter() {
@@ -8,6 +9,7 @@ export function AppRouter() {
     <Routes>
       <Route path="/" element={<Navigate to="/blog-search" replace />} />
       <Route path="/blog-search" element={<BlogSearchPage />} />
+      <Route path="/mypage" element={<MyPageJoinPage />} />
       <Route path="/playground" element={<PlaygroundPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
